a247e49e101d0db3d33e1d690156dbd34843cd6a
Sabrineh Kazemi <ksabrineh@gmail.com>
2025-01-13 15:15:39 +0330

    Update page responsive

--- app/page.js
+++ app/page.js
@@ -5,11 +5,11 @@ import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white overflow-hidden flex justify-center items-center">
-      <div className="flex w-[95%] h-full overflow-hidden flex-col">
+      <div className="flex md:!w-[95%] w-[80%] h-full overflow-hidden flex-col">
         <Header />
         <Dashboard />
       </div>
-      <div className="h-full overflow-auto shadow-lg w-[5%]">
+      <div className="h-full overflow-auto shadow-lg md:!w-[5%] w-[20%]">
         <Sidebar />
       </div>
     </div>
